91a151ab97c3c5738920ffcdb4504c84bba6898c
Greg Hendrickson <greg@hndrx.co>
2026-04-06 17:23:06 +0000

    feat(events): Scheduled events - event service, RSVP system, event creation modal and detail view

--- frontend/src/lib/components/events/index.ts
+++ frontend/src/lib/components/events/index.ts
@@ -1,3 +1,5 @@
 export { default as EventBanner } from './EventBanner.svelte';
 export { default as EventCreateModal } from './EventCreateModal.svelte';
 export { default as EventDetail } from './EventDetail.svelte';
+export { default as EventList } from './EventList.svelte';
+export { default as EventDetailView } from './EventDetailView.svelte';
